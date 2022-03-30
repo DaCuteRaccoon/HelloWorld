20b39f5cf00c53b312b2d09ab8654f51fc071725
Restyled.io <commits@restyled.io>
2022-03-30 05:54:37 +0000

    Restyled by clang-format

--- code/HelloWorld.java
+++ code/HelloWorld.java
@@ -1,9 +1,8 @@
 public class HelloWorld {
-  
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-  
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
 
 // Explanation: https://www.programiz.com/java-programming/hello-world
